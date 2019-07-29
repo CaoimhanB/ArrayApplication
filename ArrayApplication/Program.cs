9953fd95388301d77992f7a0254588bcced22e45
Caoimhan Bradley <caoimhan@alchemytechs.com>
2019-07-29 16:12:00 +0100

    Add project files.

---
+++ ArrayApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ArrayApplication
+{
+    class MyArray
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[10]; // n is an array of 10 integers
+            int i, j;
+
+            // initialise elements of array n
+            for (i = 0; i < 10; i++)
+            {
+                n[i] = i + 100;
+            }
+
+            //output each array elements value
+            for (j = 0; j < 10; j++)
+            {
+                Console.WriteLine("Element[{0}] = {1}", j, n[j]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
